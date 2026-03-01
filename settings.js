4dcaf74c6c43970271a03361638fb87081992e26
AbdulRehman19721986 <ranaabdulrehman1986@gmail.com>
2026-03-01 10:00:10 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,26 +3,26 @@ const settings = {
   packname: 'REDXBOT302',
   author: 'Abdul Rehman Rajpoot',
   timeZone: 'Asia/Karachi',
-  botName: "REDXBOT302",
+  botName: 'REDXBOT302',
   botOwner: 'Abdul Rehman Rajpoot',
-  ownerNumber: '61468259338', // primary owner
-  secondOwnerNumber: '923183928892', // secondary owner
+  ownerNumber: '61468259338',
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: 'public',
   maxStoreMessages: 20,
   tempCleanupInterval: 1 * 60 * 60 * 1000,
   storeWriteInterval: 10000,
-  description: "REDXBOT302 – Advanced WhatsApp Bot with 100+ features",
-  version: "5.2.0",
-  updateZipUrl: "https://github.com/AbdulRehman19721986/redxbot302/archive/refs/heads/main.zip",
-  channelLink: "https://whatsapp.com/channel/0029VbCPnYf96H4SNehkev10",
-  ytch: "rootmindtech",
-  pairSite: "http://redxpair.gt.tc",
-  githubRepo: "https://github.com/AbdulRehman19721986/redxbot302",
-  telegramGroup: "https://t.me/TeamRedxhacker2",
-  youtubeChannel: "https://youtube.com/@rootmindtech",
-  whatsappGroup: "https://chat.whatsapp.com/LhSmx2SeXX75r8I2bxsNDo",
-  botLogo: "https://files.catbox.moe/s36b12.jpg"
+  description: 'REDXBOT302 – Advanced WhatsApp Bot with 100+ features',
+  version: '5.2.0',
+  updateZipUrl: 'https://github.com/AbdulRehman19721986/redxbot302/archive/refs/heads/main.zip',
+  channelLink: 'https://whatsapp.com/channel/0029VbCPnYf96H4SNehkev10',
+  ytch: 'rootmindtech',
+  pairSite: 'http://redxpair.gt.tc',
+  githubRepo: 'https://github.com/AbdulRehman19721986/redxbot302',
+  telegramGroup: 'https://t.me/TeamRedxhacker2',
+  youtubeChannel: 'https://youtube.com/@rootmindtech',
+  whatsappGroup: 'https://chat.whatsapp.com/LhSmx2SeXX75r8I2bxsNDo',
+  botDp: 'https://files.catbox.moe/s36b12.jpg',
+  channelJid: '120363405513439052@newsletter'
 };
 
 module.exports = settings;

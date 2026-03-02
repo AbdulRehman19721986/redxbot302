167b47f406a5e59f1cdaa981a5c7cf89e65260de
AbdulRehman19721986 <ranaabdulrehman1986@gmail.com>
2026-03-02 16:49:14 +0000

    Update clearsession.js

--- plugins/clearsession.js
+++ plugins/clearsession.js
@@ -7,7 +7,7 @@ const channelInfo = {
     forwardingScore: 999,
     isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: '120363319098372999@newsletter',
+      newsletterJid: '120363405513439052@newsletter',
       newsletterName: 'REDXBOT302',
       serverMessageId: -1
     }
